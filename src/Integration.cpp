125d9cefad2a6b0f23bbb96c9bfd5dc0532fdd48
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-03-09 15:14:03 +0100

    Reading a complete configuration.

--- src/Integration.cpp
+++ src/Integration.cpp
@@ -27,53 +27,67 @@ std::string integrationConf::print() const {
 void readTunedIntegrationConf(tunedIntegrationConf & tunedConf, const std::string & confFilename) {
   unsigned int dim0 = 0;
   unsigned int integration = 0;
-	std::string temp;
+  std::string temp;
   std::string deviceName;
   PulsarSearch::integrationConf * parameters = 0;
-	std::ifstream confFile;
+  std::ifstream confFile;
 
   confFile.open(confFilename);
-	while ( ! confFile.eof() ) {
-		unsigned int splitPoint = 0;
+  while ( ! confFile.eof() ) {
+    unsigned int splitPoint = 0;
 
-		std::getline(confFile, temp);
-		if ( ! std::isalpha(temp[0]) ) {
-			continue;
-		}
+    std::getline(confFile, temp);
+    if ( ! std::isalpha(temp[0]) ) {
+      continue;
+    }
     parameters = new PulsarSearch::integrationConf();
 
-		splitPoint = temp.find(" ");
-		deviceName = temp.substr(0, splitPoint);
-		temp = temp.substr(splitPoint + 1);
-		splitPoint = temp.find(" ");
-		dim0 = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
-		temp = temp.substr(splitPoint + 1);
-		splitPoint = temp.find(" ");
-		integration = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
-		temp = temp.substr(splitPoint + 1);
-		splitPoint = temp.find(" ");
+    splitPoint = temp.find(" ");
+    deviceName = temp.substr(0, splitPoint);
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    dim0 = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    integration = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
     parameters->setSubbandDedispersion(isa::utils::castToType< std::string, bool >(temp.substr(0, splitPoint)));
-		temp = temp.substr(splitPoint + 1);
-		splitPoint = temp.find(" ");
-		parameters->setNrThreadsD0(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
-		temp = temp.substr(splitPoint + 1);
-		parameters->setNrItemsD0(isa::utils::castToType< std::string, unsigned int >(temp));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    parameters->setNrThreadsD0(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
+    temp = temp.substr(splitPoint + 1);
+    parameters->setNrThreadsD0(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    parameters->setNrThreadsD1(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    parameters->setNrThreadsD2(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    parameters->setNrItemsD0(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
+    temp = temp.substr(splitPoint + 1);
+    splitPoint = temp.find(" ");
+    parameters->setNrItemsD1(isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint)));
+    temp = temp.substr(splitPoint + 1);
+    parameters->setNrItemsD2(isa::utils::castToType< std::string, unsigned int >(temp));
 
-		if ( tunedConf.count(deviceName) == 0 ) {
+    if ( tunedConf.count(deviceName) == 0 ) {
       std::map< unsigned int, std::map< unsigned int, PulsarSearch::integrationConf * > * >  * externalContainer = new std::map< unsigned int, std::map< unsigned int, PulsarSearch::integrationConf * > * >();
       std::map< unsigned int, PulsarSearch::integrationConf * > * internalContainer = new std::map< unsigned int, PulsarSearch::integrationConf * >();
 
-			internalContainer->insert(std::make_pair(integration, parameters));
-			externalContainer->insert(std::make_pair(dim0, internalContainer));
-			tunedConf.insert(std::make_pair(deviceName, externalContainer));
-		} else if ( tunedConf.at(deviceName)->count(dim0) == 0 ) {
+      internalContainer->insert(std::make_pair(integration, parameters));
+      externalContainer->insert(std::make_pair(dim0, internalContainer));
+      tunedConf.insert(std::make_pair(deviceName, externalContainer));
+    } else if ( tunedConf.at(deviceName)->count(dim0) == 0 ) {
       std::map< unsigned int, PulsarSearch::integrationConf * > * internalContainer = new std::map< unsigned int, PulsarSearch::integrationConf * >();
 
-			internalContainer->insert(std::make_pair(integration, parameters));
-			tunedConf.at(deviceName)->insert(std::make_pair(dim0, internalContainer));
-		} else {
-			tunedConf.at(deviceName)->at(dim0)->insert(std::make_pair(integration, parameters));
-		}
+      internalContainer->insert(std::make_pair(integration, parameters));
+      tunedConf.at(deviceName)->insert(std::make_pair(dim0, internalContainer));
+    } else {
+      tunedConf.at(deviceName)->at(dim0)->insert(std::make_pair(integration, parameters));
+    }
   }
   confFile.close();
 }
